2a91f24c45b15c811574eda670ef5e08cf310c04
root <root@DESKTOP-U53C9JL.>
2025-01-10 06:24:34 +0000

    episode-01

---
+++ App.js
@@ -0,0 +1,35 @@
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I'm an h1 tag</h1>
+        <h2>I'm an h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>I'm an h1 tag</h1>
+        <h2>I'm an h2 tag</h2>
+    </div>
+</div>
+
+ReactElement(Object) => HTML(Browser Understands)
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "I am an h1 tag"), React.createElement('h2', {}, 'I am an h2 tag')]
+  ), 
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "I am an h1 tag"), React.createElement('h2', {}, 'I am an h2 tag')]
+  )]
+);
+
+console.log(parent); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
